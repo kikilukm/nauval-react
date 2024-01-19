14fc52fb48da78a673e41ae771b057a34e26ab3b
Kiki Lukman Hakim <kiki.lukmanhakim@gmail.com>
2024-01-19 23:40:33 +0700

    perbaikan pada package jason di lite-server

--- public/app.js
+++ public/app.js
@@ -1,3 +1,12 @@
 function Hello(props) {
   return /*#__PURE__*/React.createElement("h1", null, "Hello, ", props.name);
 }
+const root = document.querySelector("#root");
+const element = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(Hello, {
+  name: "Kiki Lukman Hakim"
+}), /*#__PURE__*/React.createElement(Hello, {
+  name: "Kiki Hakim"
+}), /*#__PURE__*/React.createElement(Hello, {
+  name: "Kiki Lukman "
+}));
+ReactDOM.render(element, root);
--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
 function Hello(props) {
-    return <h1>Hello, {props.name}</h1>;
+	return <h1>Hello, {props.name}</h1>;
 }
+
+const root = document.querySelector("#root");
+
+const element = (
+	<>
+		<Hello name="Kiki Lukman Hakim" />
+		<Hello name="Kiki Hakim" />
+		<Hello name="Kiki Lukman " />
+	</>
+);
+
+
+
+ReactDOM.render(element, root);
