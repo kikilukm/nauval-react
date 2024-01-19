3d97bc37bcf991f985db5cf951d83915ee789399
Kiki Lukman Hakim <kiki.lukmanhakim@gmail.com>
2024-01-19 23:11:54 +0700

    peneysuaian pada dependencies

---
+++ public/app.js
@@ -0,0 +1,3 @@
+function Hello(props) {
+  return /*#__PURE__*/React.createElement("h1", null, "Hello, ", props.name);
+}
---
+++ src/app.js
@@ -0,0 +1,3 @@
+function Hello(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
