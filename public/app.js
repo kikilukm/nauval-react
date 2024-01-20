6a2c567501bb0bdcab6ac0df495c93119f3225bd
Kiki Lukman Hakim <kiki.lukmanhakim@gmail.com>
2024-01-20 19:54:01 +0700

    add some feature

---
+++ public/app.js
@@ -0,0 +1,6 @@
+document.querySelector("#root");
+function App() {
+  return /*#__PURE__*/React.createElement("h1", null, "Hello World");
+}
+const element = /*#__PURE__*/React.createElement(App, null);
+createRoot(element, root);
---
+++ src/app.js
@@ -0,0 +1,9 @@
+document.querySelector("#root");
+
+function App() {
+	return <h1>Hello World</h1>;
+}
+
+const element = <App />;
+
+ReactDOM.render(element, root);
