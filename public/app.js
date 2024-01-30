2e212ec8c41f44e7dbd40905a8e94bd65bcfffb2
Kiki Lukman Hakim <kiki.lukmanhakim@gmail.com>
2024-01-30 07:52:01 +0700

    menambah css

--- public/app.js
+++ public/app.js
@@ -1,6 +1,38 @@
-document.querySelector("#root");
+const element = document.querySelector("#root");
 function App() {
-  return /*#__PURE__*/React.createElement("h1", null, "Hello World? waduh pusing aing mah kumaha di sini lur kamana wae");
+  const [activity, setActivity] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
+  function generateId() {
+    return Date.now();
+  }
+  function submitHandler(event) {
+    event.preventDefault();
+    setTodos([...todos, {
+      id: generateId(),
+      activity: activity
+    }]);
+    setActivity("");
+  }
+  function deleteHandler() {
+    console.log('hapus');
+  }
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Todo todos Lite"), /*#__PURE__*/React.createElement("form", {
+    onSubmit: submitHandler
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    placeholder: "Add your plan here",
+    value: activity,
+    onChange: function (event) {
+      setActivity(event.target.value);
+    }
+  }), /*#__PURE__*/React.createElement("button", {
+    type: "submit"
+  }, "SUBMIT")), /*#__PURE__*/React.createElement("ul", null, todos.map(function (todo) {
+    return /*#__PURE__*/React.createElement("li", {
+      key: todo.id
+    }, todo.activity, /*#__PURE__*/React.createElement("button", {
+      onClick: deleteHandler
+    }, "DELETE"));
+  })));
 }
-const element = /*#__PURE__*/React.createElement(App, null);
-ReactDOM.render(element, root);
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), element);
--- src/app.js
+++ src/app.js
@@ -1,13 +1,54 @@
-document.querySelector("#root");
+const element = document.querySelector("#root");
 
 function App() {
+	const [activity, setActivity] = React.useState("");
+	const [todos, setTodos] = React.useState([]);
+
+	function generateId() {
+		return Date.now();
+	}
+	function submitHandler(event) {
+		event.preventDefault();
+		setTodos([
+			...todos,
+			{
+				id: generateId(),
+				activity: activity,
+			},
+		]);
+		setActivity("");
+	}
+
+	function deleteHandler() {
+		console.log('hapus')
+	}
+
 	return (
-		<h1>
-			Hello World? waduh pusing aing mah kumaha di sini lur kamana wae
-		</h1>
+		<>
+			<h1>Todo todos Lite</h1>
+			<form onSubmit={submitHandler}>
+				<input
+					type="text"
+					placeholder="Add your plan here"
+					value={activity}
+					onChange={function (event) {
+						setActivity(event.target.value);
+					}}
+				></input>
+				<button type="submit">SUBMIT</button>
+			</form>
+			<ul>
+				{todos.map(function (todo) {
+					return (
+						<li key={todo.id}>
+							{todo.activity}
+							<button onClick={deleteHandler}>DELETE</button>
+						</li>
+					);
+				})}
+			</ul>
+		</>
 	);
 }
 
-const element = <App />;
-
-ReactDOM.render(element, root);
+ReactDOM.render(<App />, element);
