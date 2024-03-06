f82b6ddb2be1d68b6cc07e25744e01b3ecd68196
Kiki Lukman Hakim <kiki.lukmanhakim@gmail.com>
2024-03-06 22:43:27 +0700

    penambahan beberapa fitur hapus , edit dan update

--- src/app.js
+++ src/app.js
@@ -2,13 +2,32 @@ const element = document.querySelector("#root");
 
 function App() {
 	const [activity, setActivity] = React.useState("");
+	const [edit, setEdit] = React.useState({});
 	const [todos, setTodos] = React.useState([]);
 
 	function generateId() {
 		return Date.now();
 	}
-	function submitHandler(event) {
+	function saveTodoHandler(event) {
 		event.preventDefault();
+
+		if (edit.id) {
+			const updatedTodo = {
+				id: edit.id,
+				activity: activity,
+			};
+
+			const editTodoIndex = todos.findIndex(function (todo) {
+				return todo.id === edit.id;
+			});
+
+			const updatedTodos = [...todos];
+			updatedTodos[editTodoIndex] = updatedTodo;
+
+			setTodos(updatedTodos);
+			return cancelEditHandler();
+		}
+
 		setTodos([
 			...todos,
 			{
@@ -16,21 +35,36 @@ function App() {
 				activity: activity,
 			},
 		]);
-		setActivity("");
+
+		cancelEditHandler();
+	}
+	function removeTodoHandler(todoId) {
+		const filteredTodos = todos.filter(function (todo) {
+			return todo.id !== todoId;
+		});
+
+		setTodos(filteredTodos);
+		cancelEditHandler();
 	}
 
-	function removeTodoHandler(todoId) {
-		const filterTodos = todos.filter(function (todos) {
-			return todos.id !== todoId;
-		});
-		setTodos(filterTodos);
+	function editTodoHandler(todo) {
+		setActivity(todo.activity);
+		setEdit(todo);
+	}
+
+	function cancelEditHandler() {
+		setActivity("");
+		setEdit({});
 	}
 
 	return (
 		<>
 			<h1 className="title">Todo Lite ✅</h1>
-			<img src="https://th.bing.com/th/id/R.baa8b83e6e6ce6a2dcd86082b59effca?rik=rUAlj4EmP%2f4ZLg&riu=http%3a%2f%2f4.bp.blogspot.com%2f-8HfLPbR1n0M%2fVLREeYjL0pI%2fAAAAAAAAAb8%2fCxNo7Qn3vG0%2fs1600%2fTo-Do%2bList.png&ehk=8KCQGCqkoAv3Jb2lkwYw94QfJ71bxVu1ywp65%2b6%2bBD4%3d&risl=&pid=ImgRaw&r=0" />
-			<form onSubmit={submitHandler}>
+			<img
+				alt="nature"
+				src="https://th.bing.com/th/id/R.baa8b83e6e6ce6a2dcd86082b59effca?rik=rUAlj4EmP%2f4ZLg&riu=http%3a%2f%2f4.bp.blogspot.com%2f-8HfLPbR1n0M%2fVLREeYjL0pI%2fAAAAAAAAAb8%2fCxNo7Qn3vG0%2fs1600%2fTo-Do%2bList.png&ehk=8KCQGCqkoAv3Jb2lkwYw94QfJ71bxVu1ywp65%2b6%2bBD4%3d&risl=&pid=ImgRaw&r=0"
+			/>
+			<form onSubmit={saveTodoHandler}>
 				<input
 					type="text"
 					placeholder="Add your plan here"
@@ -40,8 +74,13 @@ function App() {
 					}}
 				></input>
 				<button className="sbmt-button" type="submit">
-					SUBMIT
+					{edit.id ? "SIMPAN PERUBAHAN" : "SUBMIT"}
 				</button>
+				{edit.id && (
+					<button className="sbmt-button" onClick={cancelEditHandler}>
+						BATAL
+					</button>
+				)}
 			</form>
 			<ul>
 				{todos.map(function (todo) {
@@ -49,17 +88,17 @@ function App() {
 						<li key={todo.id}>
 							{todo.activity}
 							<button
-								// className="button"
-								// onClick={removeTodoHandler.bind(this, todo.id)}
+								className="button"
+								onClick={editTodoHandler.bind(this, todo)}
 							>
-								{" "}
-								EDIT
+								UBAH
 							</button>
+
 							<button
 								className="button"
 								onClick={removeTodoHandler.bind(this, todo.id)}
 							>
-								DELETE
+								HAPUS
 							</button>
 						</li>
 					);
